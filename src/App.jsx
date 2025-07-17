b50eecb8c8559755e0291d67f225dd6a0af2d8e5
JobMutugi <creativejobu@gmail.com>
2025-07-17 12:27:10 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <p>This is our Project</p>  
+      
       </div>
       <h1>Vite + React</h1>
       <div className="card">
